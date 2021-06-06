fc18380b9d96837454a4a9983cbe9f029c03f17c
nivransh2008 <69744641+nivransh2008@users.noreply.github.com>
2021-06-06 16:39:12 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import BarterScreen from './BarterScreen'
+ 
+
+ export default class App extends  React.Component{
+   render(){
+     return(
+     <BarterScreen/>
+     )
+   }
+ }
